4ece1acbbc97396d064787ee61c6ea7169caa046
Alan Bignon <alan.bignon@imie.fr>
2018-11-26 15:45:02 +0100

    ajout de contact

---
+++ contact/contact.php
@@ -0,0 +1,39 @@
+<?php
+?><!DOCTYPE html>
+<html>
+<head>
+    <meta charset="uft-8">
+    <title>IT Knowledge</title>
+    <link rel="stylesheet" type="text/css" href="../version_ordi/acceuil_ordi.css">
+    <link rel="stylesheet" type="text/css" href="../ordi_header/projet_header.css">
+    <link rel="stylesheet" type="text/css" href="../contact/form.css">
+    <link rel="stylesheet" type="text/css" href="../footer/footer.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/x-icon" href="../images/logo_mobile.png" />
+</head>
+<body>
+<?php
+include ('../ordi_header/projet_header.php');
+?>
+<main>
+    <div id="bandeau_rouge">
+        <img src="../images/logo_imie.png" class="logoimie">
+        <h1 class="bvn">Bienvenue sur le blog IMIE</h1>
+    </div>
+</main>
+
+<span class="chevron">
+    <
+    </span>
+
+<?php
+include ('../contact/form.php')
+?>
+<span class="chevron2">
+    />
+    </span>
+<?php
+include ('../footer/footer.php')
+?>
+</body>
+</html>
---
+++ contact/form.php
@@ -0,0 +1,13 @@
+<div class = "container">
+    <form class ="formulaire">
+        Nom:<br>
+        <input type="text" name="firstname" class="input"><br>
+        Prénom:<br>
+        <input type="text" name="lastname" class="input"><br>
+        Adresse mail: <br>
+        <input type="email" name="mail" class="input"><br>
+        Faites nous parvenir vos commentaires : <br>
+        <input type="textarea" name="critique" class="input" id="textarea"><br>
+        <input type="submit" class="bouton">
+    </form>
+</div>
