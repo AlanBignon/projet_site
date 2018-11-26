8d3fb6f8fd69e52d88897b45e304735e0d87bf02
Alan Bignon <alan.bignon@imie.fr>
2018-11-26 16:01:43 +0100

    changement dans form

--- contact/form.php
+++ contact/form.php
@@ -1,13 +1,13 @@
 <div class = "container">
     <form class ="formulaire">
         Nom:<br>
-        <input type="text" name="firstname" class="input"><br>
+        <input title="firstname" type="text" name="firstname" class="input"><br>
         Prénom:<br>
-        <input type="text" name="lastname" class="input"><br>
+        <input title="lastname" type="text" name="lastname" class="input"><br>
         Adresse mail: <br>
-        <input type="email" name="mail" class="input"><br>
+        <input title="mail" type="email" name="mail" class="input"><br>
         Faites nous parvenir vos commentaires : <br>
-        <input type="textarea" name="critique" class="input" id="textarea"><br>
+        <input title="message" type="textarea" name="message" class="input" id="textarea"><br>
         <input type="submit" class="bouton">
     </form>
 </div>
