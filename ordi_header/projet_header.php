dea84da01e1a48dfab2694adebce0f551ddaff2d
Alan Bignon <alan.bignon@imie.fr>
2018-11-26 10:24:24 +0100

    ahout de la premiere version

---
+++ ordi_header/projet_header.php
@@ -0,0 +1,45 @@
+<header class="itk_header">
+    <div class="itk_container">
+        <div class="itk_nav_menu">
+            <div class="itk_nav_menu_img">
+                <span><img src="../images/menu.png" class="img_hamburger"></span>
+            </div>
+            <div class="itk_nav_menu_text">
+                <span class="menu">Menu</span>
+            </div>
+        </div>
+
+        <div class="logo">
+            <span><img src="../images/logo.png" class="img_logo_itk"></span>
+        </div>
+        <div class="itk_menu_deroule">
+            <div class="Actualités">
+                <span>Actualités</span>
+            </div>
+            <div class="Informatique">
+                <span>Informatique</span>
+            </div>
+            <div class="telephonie">
+                <span>Telephonie</span>
+            </div>
+            <div class="audio">
+                <span>Audio</span>
+            </div>
+            <div class="Tv_video">
+                <span>TV / video</span>
+            </div>
+                <div class="logiciels">
+                <span>Logiciels</span>
+            </div>
+        </div>
+        <div class="recherche">
+            <span><img src="../images/recherche.png" class="img_recherche"></span>
+        </div>
+        <div class="profil">
+             <span><img src="../images/profil.png" class="img_profil"></span>
+        </div>
+        <div class="compte">
+            <span>mon compte</span>
+        </div>
+    </div>
+</header>
