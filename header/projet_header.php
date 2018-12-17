bea7fe40732cbc835706ebd3db2c6fa0f41ebf71
Alan Bignon <alan.bignon@imie.fr>
2018-12-17 10:42:58 +0100

    tout bien

---
+++ header/projet_header.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <link rel="stylesheet" type="text/css" href="projet_header.css">
+    <title></title>
+</head>
+<body>
+
+<header class="itk_header">
+    <div class="itk_container">
+        <div class="itk_nav_menu">
+            <div class="itk_nav_menu_img">
+                <span><img src="images/menu.png" class="img_hamburger" alt="hamburger"></span>
+            </div>
+            <div class="itk_nav_menu_text">
+                <span class="menu">Menu</span>
+            </div>
+        </div>
+
+        <div class="logo">
+            <span><img src="images/logo.png" class="img_logo_itk" alt="logo itk"></span>
+        </div>
+        <div class="itk_menu_deroule">
+            <div class="menu_dans_le_menu">
+                <span><a href="http://localhost:8080/Projet/">Menu</a> </span>
+            </div>
+            <div class="actualites">
+                <span>Actualités</span>
+            </div>
+            <div class="informatique">
+                <span>Informatique</span>
+            </div>
+            <div class="telephonie">
+                <span>Telephonie</span>
+            </div>
+            <div class="multimedia">
+                <span>multimedia</span>
+            </div>
+                <div class="logiciels">
+                <span>Logiciels</span>
+            </div>
+        </div>
+        <div class="recherche">
+            <span><img src="images/recherche.png" class="img_recherche"></span>
+        </div>
+        <div class="profil">
+             <span><img src="images/profil.png" class="img_profil"></span>
+        </div>
+        <div class="compte">
+            <span>mon compte</span>
+        </div>
+    </div>
+</header>
